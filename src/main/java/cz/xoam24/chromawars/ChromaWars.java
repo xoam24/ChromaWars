2cd9efa6121ff6128e8da9fa671897385fbadfd3
xoam24 <infoxoam24@gmail.com>
2026-03-14 15:04:17 +0100

    init commit

---
+++ src/main/java/cz/xoam24/chromawars/ChromaWars.java
@@ -0,0 +1,17 @@
+package cz.xoam24.chromawars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChromaWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
